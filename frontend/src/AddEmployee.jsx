26e6cd450f8272eed0afae300fe79e15125eb3ec
Jay chinche <94739010+jaychinche@users.noreply.github.com>
2025-03-02 09:15:57 +0530

    Update AddEmployee.jsx

--- frontend/src/AddEmployee.jsx
+++ frontend/src/AddEmployee.jsx
@@ -61,7 +61,7 @@ export default function AddEmployee() {
     setLoading(true);
     const token = localStorage.getItem('token');
     try {
-      const response = await axios.post("http://localhost:4000/addemp", formData, {
+      const response = await axios.post("https://team-dhanush.onrender.com/addemp", formData, {
         withCredentials: true,
         headers: {
           'x-auth-token': token, 
